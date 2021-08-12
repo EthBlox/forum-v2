649bb07e2dc427aefe034a92844c52da9d2a17e3
cheb <ths2238@towerhill.org>
2021-08-12 10:19:33 -0400

    image error fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     reactStrictMode: true,
     images: {
-        domains: ['i.imgur.com', 'www.covalenthq.com', 'logos.covalenthq.com', 'lh3.googleusercontent.com']
+        domains: ['i.imgur.com', 'www.covalenthq.com', 'logos.covalenthq.com', 'lh3.googleusercontent.com'],
+        loader: "imgix",
+        path: "https://noop/"
     },
     exportPathMap: function() {
         return {
