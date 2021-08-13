379a02dae0c97bc9cf8eae65ef23bf23ad4f76c0
cheb <ths2238@towerhill.org>
2021-08-13 17:18:39 -0400

    imgix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
     reactStrictMode: true,
     images: {
         domains: ['i.imgur.com', 'www.covalenthq.com', 'logos.covalenthq.com', 'lh3.googleusercontent.com'],
+        loader: "imgix"
     },
     exportPathMap: function() {
         return {
